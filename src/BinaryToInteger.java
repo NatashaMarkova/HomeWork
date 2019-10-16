2ae1890f79048c74dda527be8a2c98540acac764
NatashaMarkova <natashamarkova91@gmail.com>
2019-10-16 23:52:35 +0300

    Homework 2

---
+++ src/BinaryToInteger.java
@@ -0,0 +1,9 @@
+public class BinaryToInteger {
+    public static void main(String args[]) {
+    int i = 101;
+    String s = "101";
+    System.out.println(i);
+        int decimal=Integer.parseInt(s,2);
+        System.out.println(decimal);
+    }}
+
